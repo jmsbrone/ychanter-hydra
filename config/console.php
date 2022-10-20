23f7e79e10f8f9d811211de30ee3dab327634cfe
jmsbrone <jms.brone@gmail.com>
2022-10-20 21:46:53 +0300

    Adding auth module migrations to migration list by default

--- config/console.php
+++ config/console.php
@@ -6,6 +6,14 @@ $config = require __DIR__ . '/common.php';
 $config = array_merge_recursive($config, [
     'id' => 'basic-console',
     'controllerNamespace' => 'app\commands',
+    'controllerMap' => [
+        'migrate' => [
+            'class' => 'yii\console\controllers\MigrateController',
+            'migrationPath' => [
+                '@app/modules/auth/migrations',
+            ],
+        ]
+    ],
 ]);
 
 return $config;
