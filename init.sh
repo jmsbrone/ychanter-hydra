0f6b88d1173d39d92b6ba9c4c9bcd532abaf076a
jmsbrone <jms.brone@gmail.com>
2022-11-15 13:29:09 +0300

    Refactoring config

--- init.sh
+++ init.sh
@@ -5,5 +5,6 @@ database="ychanter_hydra_$(echo $RANDOM | md5sum | head -c 8)"
 
 # Creating configs
 echo "DB_ROOT_PASSWORD=$rootpassword" > ".env"
+mkdir docker/db
 echo "CREATE DATABASE \"$database\";" > docker/db/init.sql
 echo "<?php return ['database'=>'$database','user'=>'postgres','password'=>'$rootpassword'];" > config/db.php
